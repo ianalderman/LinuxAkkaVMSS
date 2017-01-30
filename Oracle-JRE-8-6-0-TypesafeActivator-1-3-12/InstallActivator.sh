6450c60744653fc03d4d6fdfd0be2a403f7aac75
Ian Alderman <na>
2017-01-30 21:29:58 +0000

    Working version, added profile reload and moved downloads from /tmp to working directory

--- Oracle-JRE-8-6-0-TypesafeActivator-1-3-12/InstallActivator.sh
+++ Oracle-JRE-8-6-0-TypesafeActivator-1-3-12/InstallActivator.sh
@@ -8,20 +8,18 @@ do
   sleep 5 
 done 
 
-#sudo yum -y udpate
-
 # based on: https://www.digitalocean.com/community/tutorials/how-to-install-java-on-centos-and-fedora 
 
-
-cd /tmp
+#Install Oracle JRE
+cd ~
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-linux-x64.rpm"
 
 yum -y localinstall jre-8u60-linux-x64.rpm
 
-rm ~/jre-8u60-linux-x64.rpm
+rm jre-8u60-linux-x64.rpm
 
 #based on: https://dpastov.blogspot.co.uk/2015/10/setup-play-framework-and-typesafe-centos.html
-
+#Install Activator
 wget "https://downloads.typesafe.com/typesafe-activator/1.3.12/typesafe-activator-1.3.12.zip"
 
 unzip typesafe-activator-1.3.12.zip
@@ -31,12 +29,18 @@ rm typesafe-activator-1.3.12.zip
 
 ln -s /opt/activator-dist-1.3.12/bin/activator /usr/local/sbin/activator
 
-#export PATH=/usr/local/sbin/activator:$PATH
 echo 'pathmunge /usr/local/sbin/activator' > /etc/profile.d/activatorpath.sh
 chmod +x /etc/profile.d/activatorpath.sh
 
+#reload profile
+. /etc/profile
+
+#make default directory to host projects
 cd /var
 mkdir www
+
+#udpate system packages
+yum -y update
 
 
 
