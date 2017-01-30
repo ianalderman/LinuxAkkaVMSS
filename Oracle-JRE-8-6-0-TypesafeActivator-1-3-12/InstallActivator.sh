a506380ff994ba638a8cebc0fdf19d2f04766eb7
Ian Alderman <na>
2017-01-30 20:09:06 +0000

    another tweak for Java, export variable not working as shell needs a new script

--- Oracle-JRE-8-6-0-TypesafeActivator-1-3-12/InstallActivator.sh
+++ Oracle-JRE-8-6-0-TypesafeActivator-1-3-12/InstallActivator.sh
@@ -1,32 +1,37 @@
 
 #!/bin/bash
+
 #based on: https://msftstack.wordpress.com/2016/05/12/extension-sequencing-in-azure-vm-scale-sets/
 #wait for the Linux Diagnostic Extension to get up and running before we kick off
 while ( ! (find /var/log/azure/Microsoft.OSTCExtensions.LinuxDiagnostic/*/extension.log | xargs grep "Start mdsd"));
 do
   sleep 5 
 done 
+
+#sudo yum -y udpate
+
 # based on: https://www.digitalocean.com/community/tutorials/how-to-install-java-on-centos-and-fedora 
 
-cd ~
+
+cd /tmp
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-linux-x64.rpm"
 
 sudo yum -y localinstall jre-8u60-linux-x64.rpm
 
-rm ~/jre-8u60-linux-x64.rpm
+#rm ~/jre-8u60-linux-x64.rpm
 
 #based on: https://dpastov.blogspot.co.uk/2015/10/setup-play-framework-and-typesafe-centos.html
 
-wget https://downloads.typesafe.com/typesafe-activator/1.3.12/typesafe-activator-1.3.12.zip
+wget "https://downloads.typesafe.com/typesafe-activator/1.3.12/typesafe-activator-1.3.12.zip"
 
 unzip typesafe-activator-1.3.12.zip
 mv activator-dist-1.3.12 /opt
 
-rm typesafe-activator-1.3.12.zip
+#rm typesafe-activator-1.3.12.zip
 
 sudo ln -s /opt/activator-dist-1.3.12/bin/activator /usr/local/sbin/activator
 
-sudo export PATH=/usr/local/sbin/activator:$PATH
+export PATH=/usr/local/sbin/activator:$PATH
 
 cd /var
 mkdir www
